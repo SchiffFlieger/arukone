3f3f76e65e10c89c22805b8958a27dcdfa359dfe
Karsten Köhler <karsten.koehler95@gmail.com>
2016-02-11 16:31:41 +0100

    clean up configuration

--- src/de/dhbw/arukone/Configuration.java
+++ src/de/dhbw/arukone/Configuration.java
@@ -1,14 +1,8 @@
 package de.dhbw.arukone;
 
-/**
- * created by Karsten Köhler on 02.02.2016
- */
 public enum Configuration {
     instance;
 
-    MersenneTwisterFast twister = new MersenneTwisterFast(System.nanoTime());
-    int maximumNumberOfGenerations = 100 * 100;
-    double mutationPropability = 0.05;
     boolean isDifficult = true;
     int size = (isDifficult ? 9 : 5);
 }
