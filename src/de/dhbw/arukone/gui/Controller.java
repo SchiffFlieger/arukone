535afb688de0a8f0a52af27f355fbeb873092fe5
Karsten Köhler <karsten.koehler95@gmail.com>
2016-02-24 09:57:57 +0100

    only numeric input for text field, values between 0 and 3000 allowed

--- src/de/dhbw/arukone/gui/Controller.java
+++ src/de/dhbw/arukone/gui/Controller.java
@@ -2,8 +2,11 @@ package de.dhbw.arukone.gui;
 
 import de.dhbw.arukone.ArukoneBoard;
 import de.dhbw.arukone.reader.BoardReader;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.util.converter.NumberStringConverter;
@@ -15,6 +18,7 @@ import java.util.ResourceBundle;
  * created by Karsten Köhler on 18.02.2016
  */
 public class Controller implements Initializable {
+    public TitledPane paneOptions;
     public ComboBox chooseBoard;
     public CheckBox showSolving;
     public TextField txtSleepTime;
@@ -24,27 +28,12 @@ public class Controller implements Initializable {
     public Button btnAbout;
     public Label lblSleepTime;
     public Label lblBoard;
-    public TitledPane paneOptions;
-
 
     @Override
     public void initialize (URL location, ResourceBundle resources) {
-        txtSleepTime.textProperty().bindBidirectional(sliderSleepTime.valueProperty(), new NumberStringConverter("#### ms"));
-
-        ObservableList<ArukoneBoard> list = new BoardReader().readAllBoardsInDirectory("res/boards");
-        list.sort((o1, o2) -> {
-            if (o1.getSize() > o2.getSize()) {
-                return 1;
-            } else if (o1.getSize() < o2.getSize()) {
-                return -1;
-            } else {
-                return o1.toString().compareTo(o2.toString());
-            }
-        });
-        chooseBoard.setItems(list);
-        if (!list.isEmpty()) {
-            chooseBoard.setValue(list.get(0));
-        }
+        fillComboBoxWithBoards();
+        onlyNumericInputOnSleepTimeTextField();
+        bindTextFieldToSlider();
     }
 
     public void handleStart (ActionEvent actionEvent) {
@@ -65,5 +54,50 @@ public class Controller implements Initializable {
         } else {
             paneOptions.setDisable(true);
         }
+    }
+
+    private void bindTextFieldToSlider() {
+        txtSleepTime.textProperty().bindBidirectional(sliderSleepTime.valueProperty(), new NumberStringConverter("####"));
+    }
+
+    private void fillComboBoxWithBoards() {
+        ObservableList<ArukoneBoard> list = new BoardReader().readAllBoardsInDirectory("res/boards");
+        list.sort((o1, o2) -> {
+            if (o1.getSize() > o2.getSize()) {
+                return 1;
+            } else if (o1.getSize() < o2.getSize()) {
+                return -1;
+            } else {
+                return o1.toString().compareTo(o2.toString());
+            }
+        });
+        chooseBoard.setItems(list);
+        if (!list.isEmpty()) {
+            chooseBoard.setValue(list.get(0));
+        }
+    }
+
+    private void onlyNumericInputOnSleepTimeTextField() {
+        txtSleepTime.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (newValue.matches("[0-9]*")) {
+                    if (newValue.equals("")) {
+                        txtSleepTime.textProperty().setValue("0");
+                        return;
+                    }
+                    int value = Integer.parseInt(newValue);
+                    if (value < 0) {
+                        txtSleepTime.textProperty().setValue("0");
+                    } else if (value > 3000) {
+                        txtSleepTime.textProperty().setValue("3000");
+                    } else {
+                        txtSleepTime.textProperty().setValue(String.valueOf(value));
+                    }
+                } else {
+                    txtSleepTime.textProperty().setValue(oldValue);
+                }
+            }
+        });
     }
 }
